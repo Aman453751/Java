d1b55723a0353f67cb9438eaef90f5bb4a676efa
Aman453751 <aman.choudhary2445@gmail.com>
2026-01-28 16:09:20 +0530

    code

---
+++ j.java
@@ -0,0 +1,22 @@
+
+public class j {
+    public static void main(String[] args) {
+     
+        int intVar = 10;
+        System.out.println("Integer Variable: " + intVar);
+
+      
+        double doubleVar = 20.5;
+        System.out.println("Double Variable: " + doubleVar);
+
+    
+        char charVar = 'A';
+        System.out.println("Character Variable: " + charVar);
+
+        boolean boolVar = true;
+        System.out.println("Boolean Variable: " + boolVar);
+
+        String stringVar = "Hello, World!";
+        System.out.println("String Variable: " + stringVar);
+    }
+}
