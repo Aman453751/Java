2d4c2c5080652a00e5684d052a849fe5572c2996
Aman453751 <aman.choudhary2445@gmail.com>
2026-04-08 22:05:33 +0530

    java

---
+++ coding/LeapYear.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println("Enter the year!");
+        Scanner input = new Scanner (System.in);
+        int year = input.nextInt();
+        if(year%400==0 ||(year % 4 ==0 && year%100!=0)){
+            System.out.println("year is leap");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+
+    }
+}
